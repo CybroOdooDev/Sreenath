613504cf36e624a81a5e9f362e25c513825b99cf
Cybrosys Odoo Devs <50653951+CybroOdooDev@users.noreply.github.com>
2022-05-04 21:59:21 +0530

    Add files via upload

---
+++ dhl_connectors/dhl_connectors/models/__init__.py
@@ -0,0 +1,8 @@
+# License OPL-1 (https://www.odoo.com/documentation/14.0/legal/licenses.html#odoo-apps).
+
+from . import delivery_carrier
+from . import dhl_parcel
+from . import dhl_parcel_item
+from . import dhl_parcel_status
+from . import stock_picking
+from . import dhl_integration
