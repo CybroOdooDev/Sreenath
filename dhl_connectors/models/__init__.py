656bdf7498ae3f9ae20db7bfe315c42e43021dd5
Cybrosys Odoo Devs <50653951+CybroOdooDev@users.noreply.github.com>
2022-09-21 07:32:19 +0530

    Delete dhl_connectors directory

--- dhl_connectors/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# License OPL-1 (https://www.odoo.com/documentation/14.0/legal/licenses.html#odoo-apps).
-
-from . import delivery_carrier
-from . import dhl_parcel
-from . import dhl_parcel_item
-from . import dhl_parcel_status
-from . import stock_picking
-from . import dhl_integration
